6673ada6ff898befb21ac88811d469fcdb64aec0
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-22 17:32:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 
 def key_for_min_value(name_hash)
 lowest_value =  nil
-key = nil
+key_for_lowest_ = nil
 name_hash.each do |name, number|
   if lowest_value == nil || number < lowest_value
     lowest_value = number 
